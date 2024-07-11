4b4d12512e91a826882b9d9e138ebf850934b974
laura-benavente <laurabenaventealmazan@gmail.com>
2024-07-11 18:17:33 +0200

    memory cards fixed

--- 5/src/ui.ts
+++ 5/src/ui.ts
@@ -1,5 +1,12 @@
-import { Tablero, crearTableroInicial } from './model';
-import { voltearLaCarta, sonPareja, parejaEncontrada, parejaNoEncontrada, iniciaPartida, sePuedeVoltearLaCarta } from './motor';
+import { Carta, Tablero, crearTableroInicial } from "./model";
+import {
+  voltearLaCarta,
+  sonPareja,
+  parejaEncontrada,
+  parejaNoEncontrada,
+  iniciaPartida,
+  sePuedeVoltearLaCarta,
+} from "./motor";
 
 let tablero: Tablero = crearTableroInicial();
 
@@ -8,29 +15,37 @@ const iniciarJuego = () => {
   renderizarTablero(tablero);
 };
 
+const pintarCarta = (
+  cartaElement: HTMLDivElement,
+  carta: Carta,
+  index: number
+): HTMLDivElement => {
+  cartaElement.classList.add("carta");
+  cartaElement.dataset.index = index.toString();
+
+  if (carta.estaVuelta) {
+    cartaElement.innerHTML = `<img src=${carta.imagen} style="width: 100%; height: 100%;">`;
+    cartaElement.classList.add("flipped");
+    cartaElement.classList.remove("unflipped");
+  } else {
+    cartaElement.classList.remove("flipped");
+    cartaElement.classList.add("unflipped");
+  }
+
+  return cartaElement;
+};
+
 const renderizarTablero = (tablero: Tablero) => {
-  const tableroElement = document.getElementById('tablero');
+  const tableroElement = document.getElementById("tablero");
   if (!tableroElement) return;
 
-  tableroElement.innerHTML = '';
+  tableroElement.innerHTML = "";
   tablero.cartas.forEach((carta, index) => {
-    const cartaElement = document.createElement('div');
-    cartaElement.classList.add('carta');
-    cartaElement.dataset.index = index.toString();
-    
-    if(carta.estaVuelta) {
-      cartaElement.innerHTML = `<img src=${carta.imagen} style="width: 100%; height: 100%;">`;
-      cartaElement.classList.add('flipped');
-      cartaElement.classList.remove('unflipped');
+    const cartaElement = document.createElement("div");
 
-    } else {
-      cartaElement.classList.remove('flipped');
-      cartaElement.classList.add('unflipped');
-    }
+    tableroElement.appendChild(pintarCarta(cartaElement, carta, index));
 
-    tableroElement.appendChild(cartaElement);
-
-    cartaElement.addEventListener('click', () => manejarClickCarta(index));
+    cartaElement.addEventListener("click", () => manejarClickCarta(index));
   });
 };
 
@@ -40,7 +55,7 @@ const manejarClickCarta = (index: number) => {
   }
 
   voltearLaCarta(tablero, index);
- renderizarTablero(tablero);
+  renderizarTablero(tablero);
 
   if (tablero.estadoPartida === "DosCartasLevantadas") {
     const indiceA = tablero.indiceCartaVolteadaA!;
@@ -57,10 +72,8 @@ const manejarClickCarta = (index: number) => {
   }
 };
 
-const startButtonNode = document.getElementById('start-game')
+const startButtonNode = document.getElementById("start-game");
 
-if(startButtonNode !== undefined && startButtonNode !== null) {
-  startButtonNode.addEventListener('click', iniciarJuego);
+if (startButtonNode !== undefined && startButtonNode !== null) {
+  startButtonNode.addEventListener("click", iniciarJuego);
 }
-
-
